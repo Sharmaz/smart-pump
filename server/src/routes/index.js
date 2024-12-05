067b37645acf28af3ee84c02671c97b1be89e040
Ivan Robles <irae45@gmail.com>
2024-12-05 17:20:03 -0600

    add users api.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,1 +1,11 @@
 // Your endpoints.
+import express from 'express';
+import usersRouter from './users.router.js';
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/users', usersRouter);
+}
+
+export { routerApi };
