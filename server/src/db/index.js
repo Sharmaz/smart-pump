798bbb9661c7ff288c08dc7373652b3e33ff52de
Ivan Robles <irae45@gmail.com>
2024-12-05 00:22:55 -0600

    add database connection.

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,1 +1,15 @@
 // For migrations, models and seeders. Also db connection.
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import { JSONFilePreset } from 'lowdb/node';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const data = readFileSync(join(__dirname, './users.json'));
+
+const file = join(__dirname, './db.json');
+const defaultData = JSON.parse(data);
+const db = await JSONFilePreset(file, defaultData);
+await db.write()
+
+export { db };
